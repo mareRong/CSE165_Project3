6cb3ffcf4972d4ff66302f7f20df09fad9bc5aeb
Ashish Bamba <153144074+AshishBamba05@users.noreply.github.com>
2026-05-15 10:16:03 -0700

    Leveled avatar more with ground by -15 y

--- Assets/Scripts/SpatialSurfaceAnchorManager.cs
+++ Assets/Scripts/SpatialSurfaceAnchorManager.cs
@@ -25,7 +25,7 @@ public sealed class SpatialSurfaceAnchorManager : MonoBehaviour
     [SerializeField] private bool _buildOnStart = true;
     [SerializeField] private bool _rebuildExistingSurfaces = true;
     [SerializeField] private bool _addMeshColliders = true;
-    [SerializeField] private float _floorWorldY = -8f;
+    [SerializeField] private float _floorWorldY = -15f;
     [SerializeField] private SurfaceDefinition[] _surfaces =
     {
         new SurfaceDefinition
