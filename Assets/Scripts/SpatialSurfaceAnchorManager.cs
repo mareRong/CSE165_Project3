d84b705c8a860fd9886b2e4fece3daf91d57930b
Ashish Bamba <153144074+AshishBamba05@users.noreply.github.com>
2026-05-19 21:41:29 -0700

    Debug spatial anchors on main

--- Assets/Scripts/SpatialSurfaceAnchorManager.cs
+++ Assets/Scripts/SpatialSurfaceAnchorManager.cs
@@ -28,9 +28,9 @@ public sealed class SpatialSurfaceAnchorManager : MonoBehaviour
     [SerializeField] private bool _rebuildExistingSurfaces = true;
     [SerializeField] private bool _addMeshColliders = true;
     [SerializeField] private bool _useDetectedRoomWalls = true;
-    [SerializeField] private bool _showConfiguredWallsWhileDetecting = false;
-    [SerializeField] private bool _requestSceneCaptureIfNoRoom = false;
-    [SerializeField] private bool _useConfiguredWallsWhenDetectionFails = false;
+    [SerializeField] private bool _showConfiguredWallsWhileDetecting = true;
+    [SerializeField] private bool _requestSceneCaptureIfNoRoom = true;
+    [SerializeField] private bool _useConfiguredWallsWhenDetectionFails = true;
     [SerializeField] private int _detectedRoomWallFetchAttempts = 20;
     [SerializeField] private float _detectedRoomWallFetchRetryDelaySeconds = 0.75f;
     [SerializeField] private Color _wallOverlayColor = new Color(1f, 0.82f, 0f, 0.72f);
